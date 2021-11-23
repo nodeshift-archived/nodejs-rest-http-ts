7b026a00a40930494ac13aae27e42140cce43ae0
Helio Frota <00hf11@gmail.com>
2021-11-23 08:21:58 -0300

    chore: some updates in code and deps

--- src/app.ts
+++ src/app.ts
@@ -20,12 +20,11 @@
 
 import path from 'path';
 import express from 'express';
-import bodyParser from 'body-parser';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/licenses', express.static(path.join(__dirname, 'licenses')));
 
